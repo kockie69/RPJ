21730a92171d8432233ed52c66b151d3ed4b08d5
robert Kock <robert.kock@gmail.com>
2021-06-23 11:01:12 +0100

    First upload

---
+++ src/RPJ.hpp
@@ -0,0 +1,138 @@
+#include "rack.hpp"
+using namespace rack;
+extern Plugin *pluginInstance;
+extern Model *modelLFO;
+
+struct ATextLabel : TransparentWidget {
+	std::shared_ptr<Font> font;
+	NVGcolor txtCol;
+	char text[128];
+	const int fh = 14;
+
+	ATextLabel(Vec pos) {
+		box.pos = pos;
+		box.size.y = fh;
+		setColor(0x00, 0x00, 0x00, 0xFF);
+		font = APP->window->loadFont(asset::plugin(pluginInstance, "res/DejaVuSansMono.ttf"));
+		setText(" ");
+	}
+
+	ATextLabel(Vec pos, unsigned char r, unsigned char g, unsigned char b, unsigned char a) {
+		box.pos = pos;
+		box.size.y = fh;
+		setColor(r, g, b, a);
+		font = APP->window->loadFont(asset::plugin(pluginInstance, "res/DejaVuSansMono.ttf"));
+		setText(" ");
+	}
+
+	void setColor(unsigned char r, unsigned char g, unsigned char b, unsigned char a) {
+		txtCol.r = r;
+		txtCol.g = g;
+		txtCol.b = b;
+		txtCol.a = a;
+	}
+
+	void setText(const char * txt) {
+		strncpy(text, txt, sizeof(text));
+		box.size.x = strlen(text) * 8;
+	}
+
+	void drawBG(const DrawArgs &args) {
+		Vec c = Vec(box.size.x/2, box.size.y);
+		const int whalf = box.size.x/2;
+
+		// Draw rectangle
+		nvgFillColor(args.vg, nvgRGBA(0xF0, 0xF0, 0xF0, 0xFF));
+		{
+			nvgBeginPath(args.vg);
+			nvgMoveTo(args.vg, c.x -whalf, c.y +2);
+			nvgLineTo(args.vg, c.x +whalf, c.y +2);
+			nvgLineTo(args.vg, c.x +whalf, c.y+fh+2);
+			nvgLineTo(args.vg, c.x -whalf, c.y+fh+2);
+			nvgLineTo(args.vg, c.x -whalf, c.y +2);
+			nvgClosePath(args.vg);
+		}
+		nvgFill(args.vg);
+	}
+
+	void drawTxt(const DrawArgs &args, const char * txt) {
+
+		Vec c = Vec(box.size.x/2, box.size.y);
+
+		nvgFontSize(args.vg, fh);
+		nvgFontFaceId(args.vg, font->handle);
+		nvgTextLetterSpacing(args.vg, -2);
+		nvgTextAlign(args.vg, NVG_ALIGN_CENTER);
+		nvgFillColor(args.vg, nvgRGBA(txtCol.r, txtCol.g, txtCol.b, txtCol.a));
+
+		nvgText(args.vg, c.x, c.y+fh, txt, NULL);
+	}
+
+	void draw(const DrawArgs &args) override {
+		TransparentWidget::draw(args);
+		drawBG(args);
+		drawTxt(args, text);
+	}
+
+
+
+};
+
+struct ATitle: TransparentWidget {
+	std::shared_ptr<Font> font;
+	NVGcolor txtCol;
+	char text[128];
+	int fh = 20;
+	float parentW = 0;
+
+	ATitle(float pW) {
+		parentW = pW;
+		box.pos = Vec(1 , 1);
+		box.size.y = fh;
+		setColor(0x55, 0x99, 0xFF, 0xFF);
+		font = APP->window->loadFont(asset::plugin(pluginInstance, "res/DejaVuSans.ttf"));
+		setText(" ");
+	}
+
+	void setColor(unsigned char r, unsigned char g, unsigned char b, unsigned char a) {
+		txtCol.r = r;
+		txtCol.g = g;
+		txtCol.b = b;
+		txtCol.a = a;
+	}
+
+	void setText(const char * txt) {
+		strncpy(text, txt, sizeof(text));
+		box.size.x = strlen(text) * 10;
+	}
+
+	void draw(const DrawArgs &args) override {
+		TransparentWidget::draw(args);
+		drawTxt(args, text);
+	}
+
+	void drawTxt(const DrawArgs &args, const char * txt) {
+		float bounds[4];
+		Vec c = Vec(box.pos.x, box.pos.y);
+
+		nvgFontSize(args.vg, fh);
+		nvgFontFaceId(args.vg, font->handle);
+		nvgTextLetterSpacing(args.vg, -2);
+		nvgTextAlign(args.vg, NVG_ALIGN_LEFT);
+
+		// CHECK WHETHER TEXT FITS IN THE MODULE
+		nvgTextBounds(args.vg, c.x, c.y, txt, NULL, bounds);
+		float xmax = bounds[2];
+		if (xmax > parentW) {
+			float ratio = parentW / xmax;
+			fh = (int)floor(ratio * fh); // reduce fontsize to fit the parent width
+		} else {
+			c.x += (parentW - xmax)/2; // center text
+		}
+
+		nvgFillColor(args.vg, nvgRGBA(txtCol.r, txtCol.g, txtCol.b, txtCol.a));
+		nvgText(args.vg, c.x, c.y+fh, txt, NULL);
+	}
+
+};
+
