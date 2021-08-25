e23d3affc2e47c9d68781b12db40f13cc140d6f0
kockie69 <robert.kock@gmail.com>
2021-08-25 10:39:42 +0100

    Added more audio formats and backwards

--- src/Audio.cpp
+++ src/Audio.cpp
@@ -11,6 +11,8 @@ Audio::Audio() {
 	samplePos=0;
 	peak=0;
 	panningType=CONSTPOWER;
+	scaleFac = 1;
+	repeat = true;
 }
 
 void Audio::setParameters(const AudioParameters& params) {
@@ -19,12 +21,29 @@ void Audio::setParameters(const AudioParameters& params) {
     panningValue = params.panningValue;  
     rackSampleRate = params.rackSampleRate;
 	stop = params.stop;
+	if (!play)
+		play = params.play;
+	repeat = params.repeat;
+	beginRatio = params.startRatio;
+	endRatio = params.endRatio;
 }
 
-void Audio::loadSample(char *path) {
+bool Audio::loadSample(char *path) {
 	loading = true;
-	float* pSampleData = drwav_open_file_and_read_pcm_frames_f32(path, &channels, &sampleRate, &totalPCMFrameCount, NULL);
-    if (pSampleData != NULL) {
+	float* pSampleData = NULL;
+
+	if (rack::string::filenameExtension(path)=="flac")
+		pSampleData = drflac_open_file_and_read_pcm_frames_f32(path, &channels, &sampleRate, &totalPCMFrameCount, NULL);
+	else if (rack::string::filenameExtension(path)=="wav")
+		pSampleData = drwav_open_file_and_read_pcm_frames_f32(path, &channels, &sampleRate, &totalPCMFrameCount, NULL);
+    else if (rack::string::filenameExtension(path)=="mp3")
+	{
+		pSampleData = drmp3_open_file_and_read_pcm_frames_f32(path, &mp3config, &totalPCMFrameCount, NULL);
+		channels = mp3config.channels;
+		sampleRate = mp3config.sampleRate;
+	}
+	
+	if (pSampleData != NULL) {
 		playBuffer[0].clear();
 		playBuffer[1].clear();
 		for (unsigned int i=0; i < (totalPCMFrameCount*channels); i = i + channels) {
@@ -35,22 +54,40 @@ void Audio::loadSample(char *path) {
 				peak = max(abs(pSampleData[i]),abs(pSampleData[i+1]));	
 		}
         fileLoaded=true;
-		play=true;
+		if (samplePos==0)
+			samplePos = (totalPCMFrameCount*beginRatio/1024);
 	}
 	else 
 		fileLoaded = false;
 	loading = false;
+	return fileLoaded;
 }
 
 void Audio::processAudioSample() {
-    if (peak>0) {
-	    scaleFac = (float)(pow(10.0,dB/peak)*20); 
-    }
 	
 	if (stop)
 		play=false;
-		
-    if ((!loading) && (play) && ((floor(samplePos) < totalPCMFrameCount) && (floor(samplePos) >= 0))) {	
+
+	// Determine if we are going up or down
+	//float beginPos = totalPCMFrameCount*(beginRatio/1024);
+	//float endPos = totalPCMFrameCount*(endRatio/1024);
+	up = (endRatio >= beginRatio);
+	
+	// First check if samplePos is still within begin and end bounderies, otherwise return to start
+    if ((!loading) && (play) && (((up) && (floor(samplePos) <= (totalPCMFrameCount * endRatio/1024))) || ((!up) && (floor(samplePos) >= totalPCMFrameCount * endRatio/1024)))) {
+  
+		if (peak>0) {
+	    	scaleFac = (float)(pow(10.0,dB/peak)*20); 
+    	}
+
+		// if we are going up and beginPos has moved forward, change samplePos
+		if (up && samplePos < totalPCMFrameCount * beginRatio/1024)
+			samplePos = totalPCMFrameCount * beginRatio/1024;
+
+		// if we are going down and endPos has moved backwards, change samplePos
+		if (!up && samplePos > totalPCMFrameCount * beginRatio/1024)
+			samplePos = totalPCMFrameCount * beginRatio/1024;
+
 		if (channels == 1) {
 			left = panning(panningType, panningValue).left * scaleFac * (playBuffer[0][floor(samplePos)]);
 			right = panning(panningType, panningValue).right * scaleFac * (playBuffer[0][floor(samplePos)]);
@@ -59,11 +96,22 @@ void Audio::processAudioSample() {
 			left = scaleFac * panning(panningType, panningValue).left * (playBuffer[0][floor(samplePos)]);
 			right = scaleFac * panning(panningType, panningValue).right * (playBuffer[1][floor(samplePos)]);
        	}
-		samplePos=samplePos+sampleRate/rackSampleRate;
+		if (up)
+			samplePos=samplePos+(sampleRate/rackSampleRate);
+		else
+			samplePos=samplePos-(sampleRate/rackSampleRate);
 	}
-	if (floor(samplePos) >= totalPCMFrameCount) {
-		samplePos = 0;
-		play=false;
+	else {
+		if (up && play) {
+			samplePos = totalPCMFrameCount * (beginRatio/1024);
+		}
+		if (!up && play) {
+			samplePos = totalPCMFrameCount * (beginRatio/1024);
+		}
+
+		// remove next line if repeat added to parameters
+		if (!repeat)
+			play=false;
 	}
 }
 
