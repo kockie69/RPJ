0b46e7d0e8fd4cbb169254d4c9bf1b52f6d487c3
kockie69 <robert.kock@gmail.com>
2021-11-27 09:15:15 +0000

    removed some unused code

--- src/RPJ.hpp
+++ src/RPJ.hpp
@@ -21,53 +21,5 @@ extern Model *modelCircularRide;
 extern Model *modelDrillingHoles;
 extern Model *modelTuxOn;
 
-struct RPJTextLabel : TransparentWidget {
-
-	RPJTextLabel(Vec);
-
-	RPJTextLabel(Vec, int);
-
-	RPJTextLabel(Vec, int, int);
-
-	RPJTextLabel(Vec, unsigned char, unsigned char, unsigned char, unsigned char);
-
-	void setColor(unsigned char, unsigned char, unsigned char, unsigned char);
-
-	void setText(const char *);
-
-	void drawBG(const DrawArgs &);
-
-	void drawTxt(const DrawArgs &, const char *);
-
-	void draw(const DrawArgs &) override;
-
-	std::shared_ptr<Font> font;
-	NVGcolor txtCol;
-	char text[128];
-	int fh=14;
-	int moduleWidth;
-};
-
-struct RPJTitle: TransparentWidget {
-	
-	RPJTitle(float , int );
-
-	void setColor(unsigned char , unsigned char , unsigned char , unsigned char );
-
-	void setText(const char * );
-
-	void draw(const DrawArgs &) override;
-
-	void drawTxt(const DrawArgs &, const char * );
-
-	std::shared_ptr<Font> font;
-	NVGcolor txtCol;
-	//char text[128];
-	char text[128];
-	int fh = 25;
-	float parentW = 0;
-	int moduleWidth;
-};
-
 
 
